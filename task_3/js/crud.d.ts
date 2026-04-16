0f427eb719287d8f406b60b515cf27890a3170ac
root <root@ip-10-42-171-20.ec2.internal>
2026-04-16 14:36:08 +0000

    create all the file

---
+++ task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import { RowID, RowElement } from './main';
+
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
